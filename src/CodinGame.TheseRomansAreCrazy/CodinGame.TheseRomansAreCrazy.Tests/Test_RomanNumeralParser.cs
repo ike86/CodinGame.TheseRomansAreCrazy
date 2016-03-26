3f378cbf91575b94da99c1360f311c1a5472a5ec
Pfeff Ákos György <pfeffa@sonrisa.hu>
2016-03-26 17:40:41 +0100

    implemented I-III

---
+++ src/CodinGame.TheseRomansAreCrazy/CodinGame.TheseRomansAreCrazy.Tests/Test_RomanNumeralParser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodinGame.TheseRomansAreCrazy.Tests
+{
+    public class Test_RomanNumeralParser
+    {
+        [TestClass]
+        public class Parse
+        {
+            [TestMethod]
+            public void ReturnsOne_If_NumeralIsI()
+            {
+                var subject = CreateSubject();
+
+                var result = subject.Parse("I");
+
+                result.Should().Be(1);
+            }
+
+            [TestMethod]
+            public void ReturnsTwo_If_NumeralIsII()
+            {
+                var subject = CreateSubject();
+
+                var result = subject.Parse("II");
+
+                result.Should().Be(2);
+            }
+
+            [TestMethod]
+            public void ReturnsThree_If_NumeralIsIII()
+            {
+                var subject = CreateSubject();
+
+                var result = subject.Parse("III");
+
+                result.Should().Be(3);
+            }
+
+            private static RomanNumeralParser CreateSubject()
+            {
+                return new RomanNumeralParser();
+            }
+        }
+    }
+}
---
+++ src/CodinGame.TheseRomansAreCrazy/CodinGame.TheseRomansAreCrazy/RomanNumeralParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodinGame.TheseRomansAreCrazy
+{
+    public class RomanNumeralParser
+    {
+        public int Parse(string numeral)
+        {
+            return numeral.Count(ch => ch == 'I');
+        }
+    }
+}
