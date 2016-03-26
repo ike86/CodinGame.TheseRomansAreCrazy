e4388dca0e17a514ae0f91f33a77643dffc91222
Pfeff Ákos György <pfeffa@sonrisa.hu>
2016-03-26 17:18:14 +0100

    created solution skeleton

---
+++ src/CodinGame.TheseRomansAreCrazy/CodinGame.TheseRomansAreCrazy/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CodinGame.TheseRomansAreCrazy
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string rom1 = Console.ReadLine();
+            string rom2 = Console.ReadLine();
+        }
+    }
+}
