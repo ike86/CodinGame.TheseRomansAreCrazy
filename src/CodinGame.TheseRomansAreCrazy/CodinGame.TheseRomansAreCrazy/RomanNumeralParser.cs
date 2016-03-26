cf87e2b143ac4fadb626508225ed86d2596844b1
Pfeff Ákos György <pfeffa@sonrisa.hu>
2016-03-26 19:55:29 +0100

    ReturnsFour_If_NumeralIs_IV passes

--- src/CodinGame.TheseRomansAreCrazy/CodinGame.TheseRomansAreCrazy/RomanNumeralParser.cs
+++ src/CodinGame.TheseRomansAreCrazy/CodinGame.TheseRomansAreCrazy/RomanNumeralParser.cs
@@ -11,9 +11,21 @@ namespace CodinGame.TheseRomansAreCrazy
         public int Parse(string numeral)
         {
             var result = 0;
-            foreach (var ch in numeral)
+            for (int i = 0; i < numeral.Count(); i++)
             {
-                if (ch == 'V')
+                var ch = numeral[i];
+                var nextCh = char.MinValue;
+                if (i + 1 < numeral.Count())
+                {
+                    nextCh = numeral[i + 1];
+                }
+
+                if (i + 1 < numeral.Count() && ch == 'I' && nextCh == 'V')
+                {
+                    result += 4;
+                    i++;
+                }
+                else if (ch == 'V')
                     result += 5;
                 else if (ch == 'I')
                     result += 1;
