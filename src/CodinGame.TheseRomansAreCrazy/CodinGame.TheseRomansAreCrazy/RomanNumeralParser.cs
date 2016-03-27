5e8851904e1e861cded40f43b2f9754e24e595f6
Pfeff Ákos György <pfeffa@sonrisa.hu>
2016-03-27 21:21:35 +0200

    refactored token parsers

--- src/CodinGame.TheseRomansAreCrazy/CodinGame.TheseRomansAreCrazy/RomanNumeralParser.cs
+++ src/CodinGame.TheseRomansAreCrazy/CodinGame.TheseRomansAreCrazy/RomanNumeralParser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,14 +9,29 @@ namespace CodinGame.TheseRomansAreCrazy
 {
     public class RomanNumeralParser
     {
-        private IEnumerable<Func<string, Tuple<string, int>>> tokenParsers =
-            new Func<string, Tuple<string, int>>[]
+        private static TokenParser FourParser =
+            new TokenParser(
+                    s => s.FirstOrDefault() == 'I' && s.Count() > 1 && s[1] == 'V',
+                    s => new Tuple<string, int>(s.Substring(2), 4));
+
+        private static TokenParser FiveParser =
+            new TokenParser(
+                    s => s.FirstOrDefault() == 'V',
+                    s => new Tuple<string, int>(s.Substring(1), 5));
+
+        private static TokenParser OneParser =
+            new TokenParser(
+                    s => s.FirstOrDefault() == 'I',
+                    s => new Tuple<string, int>(s.Substring(1), 1));
+
+        private IEnumerable<TokenParser> tokenParsers =
+            new TokenParser[]
             {
-                ParseFour,
-                ParseFive,
-                ParseOne,
-                ParseOne,
-                ParseOne
+                FourParser,
+                FiveParser,
+                OneParser,
+                OneParser,
+                OneParser
             };
 
         public int Parse(string numeral)
@@ -24,41 +40,50 @@ namespace CodinGame.TheseRomansAreCrazy
             var remainingNumeral = numeral;
             foreach (var tokenParser in this.tokenParsers)
             {
-                var tokenParsingResult = tokenParser(remainingNumeral);
-                remainingNumeral = tokenParsingResult.Item1;
-                result += tokenParsingResult.Item2;
+                if (tokenParser.CanParse(remainingNumeral))
+                {
+                    var tokenParsingResult = tokenParser.Parse(remainingNumeral);
+                    remainingNumeral = tokenParsingResult.Item1;
+                    result += tokenParsingResult.Item2;
+                }
             }
 
             return result;
         }
-        
+
         private static Tuple<string, int> Identity(string s)
         {
             return new Tuple<string, int>(s, 0);
         }
 
-        private static Tuple<string, int> ParseFour(string s)
+        class TokenParser
         {
-            if (s.FirstOrDefault() == 'I' && s.Count() > 1 && s[1] == 'V')
-                return new Tuple<string, int>(s.Substring(2), 4);
-            else
-                return Identity(s);
-        }
+            private readonly Func<string, bool> canParse;
+            private readonly Func<string, Tuple<string, int>> parse;
 
-        private static Tuple<string, int> ParseFive(string s)
-        {
-            if (s.FirstOrDefault() == 'V')
-                return new Tuple<string, int>(s.Substring(1), 5);
-            else
-                return Identity(s);
-        }
+            public TokenParser(
+                Expression<Func<string, bool>> canParse,
+                Func<string, Tuple<string, int>> parse)
+            {
+                this.canParse = canParse.Compile();
+                this.parse = parse;
+            }
 
-        private static Tuple<string, int> ParseOne(string s)
-        {
-            if (s.FirstOrDefault() == 'I')
-                return new Tuple<string, int>(s.Substring(1), 1);
-            else
-                return Identity(s);
+            public Func<string, Tuple<string, int>> Parse
+            {
+                get
+                {
+                    return parse;
+                }
+            }
+
+            public Func<string, bool> CanParse
+            {
+                get
+                {
+                    return canParse;
+                }
+            }
         }
     }
 }
