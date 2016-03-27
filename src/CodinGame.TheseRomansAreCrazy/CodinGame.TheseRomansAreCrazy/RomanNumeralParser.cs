deeb4b9b107697b545a599eda02aac1423aad146
Pfeff Ákos György <pfeffa@sonrisa.hu>
2016-03-27 20:57:09 +0200

    refactored Parse()

--- src/CodinGame.TheseRomansAreCrazy/CodinGame.TheseRomansAreCrazy/RomanNumeralParser.cs
+++ src/CodinGame.TheseRomansAreCrazy/CodinGame.TheseRomansAreCrazy/RomanNumeralParser.cs
@@ -8,30 +8,57 @@ namespace CodinGame.TheseRomansAreCrazy
 {
     public class RomanNumeralParser
     {
+        private IEnumerable<Func<string, Tuple<string, int>>> tokenParsers =
+            new Func<string, Tuple<string, int>>[]
+            {
+                ParseFour,
+                ParseFive,
+                ParseOne,
+                ParseOne,
+                ParseOne
+            };
+
         public int Parse(string numeral)
         {
             var result = 0;
-            for (int i = 0; i < numeral.Count(); i++)
+            var remainingNumeral = numeral;
+            foreach (var tokenParser in this.tokenParsers)
             {
-                var ch = numeral[i];
-                var nextCh = char.MinValue;
-                if (i + 1 < numeral.Count())
-                {
-                    nextCh = numeral[i + 1];
-                }
-
-                if (i + 1 < numeral.Count() && ch == 'I' && nextCh == 'V')
-                {
-                    result += 4;
-                    i++;
-                }
-                else if (ch == 'V')
-                    result += 5;
-                else if (ch == 'I')
-                    result += 1;
+                var tokenParsingResult = tokenParser(remainingNumeral);
+                remainingNumeral = tokenParsingResult.Item1;
+                result += tokenParsingResult.Item2;
             }
 
             return result;
+        }
+        
+        private static Tuple<string, int> Identity(string s)
+        {
+            return new Tuple<string, int>(s, 0);
+        }
+
+        private static Tuple<string, int> ParseFour(string s)
+        {
+            if (s.FirstOrDefault() == 'I' && s.Count() > 1 && s[1] == 'V')
+                return new Tuple<string, int>(s.Substring(2), 4);
+            else
+                return Identity(s);
+        }
+
+        private static Tuple<string, int> ParseFive(string s)
+        {
+            if (s.FirstOrDefault() == 'V')
+                return new Tuple<string, int>(s.Substring(1), 5);
+            else
+                return Identity(s);
+        }
+
+        private static Tuple<string, int> ParseOne(string s)
+        {
+            if (s.FirstOrDefault() == 'I')
+                return new Tuple<string, int>(s.Substring(1), 1);
+            else
+                return Identity(s);
         }
     }
 }
